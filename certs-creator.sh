45ea16ee65ad7b1510626785217def62af4c8def
Lucas Ferraro <ferraro.luc@outlook.com>
2024-03-25 16:39:58 -0300

    fixed certs creator

--- certs-creator.sh
+++ certs-creator.sh
@@ -15,7 +15,6 @@ case $OPTION in
 esac
 
 SECRET=$(pwgen -1)
-YEAR=$(date +"%Y")
 
 if [ "$USER_TYPE" = "owner" ]; then
     CERT_TYPE=template-client-owner
@@ -23,12 +22,12 @@ else
     CERT_TYPE=template-client
 fi
 
-TEMPLATE=$(sed -e "s/\$NAME/$NAME/g" -e "s/\$YEAR/$YEAR/g" -e "s/\$SECRET/$SECRET/g" -e "s/\$CERT_TYPE/$CERT_TYPE/g" -e "s/\$USER_TYPE/$USER_TYPE/g" <<EOF
-/certificate add copy-from=$CERT_TYPE name=$NAME-$YEAR common-name=$NAME@DOMAIN.com subject-alt-name=email:$NAME@DOMAIN.com
-/certificate sign $NAME-$YEAR ca=ca
-/certificate set trusted=yes $NAME-$YEAR
-/certificate export-certificate $NAME-$YEAR type=pkcs12 export-passphrase=$SECRET
-/ip ipsec identity add auth-method=digital-signature certificate=server generate-policy=port-strict match-by=certificate mode-config=modeconfig-ikev2-$USER_TYPE peer=peer-wan policy-template-group=group-ikev2 remote-certificate=$NAME-$YEAR remote-id=user-fqdn:$NAME@DOMAIN.com
+TEMPLATE=$(sed -e "s/\$NAME/$NAME/g" -e "s/\$SECRET/$SECRET/g" -e "s/\$CERT_TYPE/$CERT_TYPE/g" -e "s/\$USER_TYPE/$USER_TYPE/g" <<EOF
+/certificate add copy-from=$CERT_TYPE name=$NAME common-name=$NAME@DOMAIN.com subject-alt-name=email:$NAME@DOMAIN.com
+/certificate sign $NAME ca=ca
+/certificate set trusted=yes $NAME
+/certificate export-certificate $NAME type=pkcs12 export-passphrase=$SECRET
+/ip ipsec identity add auth-method=digital-signature certificate=server generate-policy=port-strict match-by=certificate mode-config=modeconfig-ikev2-$USER_TYPE peer=peer-wan policy-template-group=group-ikev2 remote-certificate=$NAME remote-id=user-fqdn:$NAME@DOMAIN.com
 EOF
 )
 
@@ -38,6 +37,6 @@ if [ ! -d "Certs" ]; then
     mkdir Certs
 fi
 sftp user@192.168.1.1:cert_export_ca.crt && mv cert_export_ca.crt Certs/
-sftp user@192.168.1.1:cert_export_$NAME-$YEAR.p12 && mv cert_export_$NAME-$YEAR.p12 Certs/
+sftp user@192.168.1.1:cert_export_$NAME.p12 && mv cert_export_$NAME.p12 Certs/
 
 echo "Ready. Secret is \"$SECRET\" and the necessary files were downloaded to the \"Certs\" folder."
--- certs-updater.sh
+++ certs-updater.sh
@@ -15,7 +15,6 @@ case $OPTION in
 esac
 
 SECRET=$(pwgen -1)
-YEAR=$(date +"%Y")
 
 if [ "$USER_TYPE" = "owner" ]; then
     CERT_TYPE=template-client-owner
@@ -23,12 +22,13 @@ else
     CERT_TYPE=template-client
 fi
 
-TEMPLATE=$(sed -e "s/\$NAME/$NAME/g" -e "s/\$YEAR/$YEAR/g" -e "s/\$SECRET/$SECRET/g" -e "s/\$CERT_TYPE/$CERT_TYPE/g" -e "s/\$USER_TYPE/$USER_TYPE/g" <<EOF
-/certificate add copy-from=$CERT_TYPE name=$NAME-$YEAR common-name=$NAME@DOMAIN.com subject-alt-name=email:$NAME@DOMAIN.com
-/certificate sign $NAME-$YEAR ca=ca
-/certificate set trusted=yes $NAME-$YEAR
-/certificate export-certificate $NAME-$YEAR type=pkcs12 export-passphrase=$SECRET
-/ip ipsec identity set [find remote-id="user-fqdn:$NAME@DOMAIN.com"] mode-config="modeconfig-ikev2-$USER_TYPE" remote-certificate=$NAME-$YEAR
+TEMPLATE=$(sed -e "s/\$NAME/$NAME/g" -e "s/\$SECRET/$SECRET/g" -e "s/\$CERT_TYPE/$CERT_TYPE/g" -e "s/\$USER_TYPE/$USER_TYPE/g" <<EOF
+/certificate remove $NAME
+/certificate add copy-from=$CERT_TYPE name=$NAME common-name=$NAME@DOMAIN.com subject-alt-name=email:$NAME@DOMAIN.com
+/certificate sign $NAME ca=ca
+/certificate set trusted=yes $NAME
+/certificate export-certificate $NAME type=pkcs12 export-passphrase=$SECRET
+/ip ipsec identity set [find remote-id="user-fqdn:$NAME@DOMAIN.com"] mode-config="modeconfig-ikev2-$USER_TYPE" remote-certificate=$NAME
 EOF
 )
 
@@ -38,6 +38,6 @@ if [ ! -d "Certs" ]; then
     mkdir Certs
 fi
 sftp user@192.168.1.1:cert_export_ca.crt && mv cert_export_ca.crt Certs/
-sftp user@192.168.1.1:cert_export_$NAME-$YEAR.p12 && mv cert_export_$NAME-$YEAR.p12 Certs/
+sftp user@192.168.1.1:cert_export_$NAME.p12 && mv cert_export_$NAME.p12 Certs/
 
 echo "Done. The secret is \"$SECRET\" and the necessary files have been downloaded to the \"Certs\" folder."
